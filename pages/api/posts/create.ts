d3cf48aa1300f78aab1119cbcbe0314f2192f30a
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-31 20:23:51 +0500

    US-3: Add API to create a new blog post

---
+++ pages/api/posts/create.ts
@@ -0,0 +1,21 @@
+// pages/api/posts/create.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('./blog.db');
+
+export default function handler(
+    req: NextApiRequest, 
+    res: NextApiResponse) {
+
+
+    const { title, summary, content } = req.body;
+    
+    db.run('INSERT INTO posts (title, summary, content) VALUES (?, ?, ?)', [title, summary, content], function (err) {
+        if (err) {
+            res.status(500).json({ error: 'Database error' });
+        } else {
+            res.status(200).json({ id: this.lastID });
+        }
+    });
+}
